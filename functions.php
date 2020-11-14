15e0aa51a95f0f899d9b9e9fc32fd83030c1d52f
Kjell Reigstad <kjell@kjellr.com>
2020-11-14 14:28:13 -0500

    Rename

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( ! function_exists( 'kjellr_support' ) ) :
-	function kjellr_support()  {
+if ( ! function_exists( 'kjell_blocks_support' ) ) :
+	function kjell_blocks_support()  {
 
 		// Alignwide and alignfull classes in the block editor
 		add_theme_support( 'align-wide' );
@@ -24,18 +24,18 @@ if ( ! function_exists( 'kjellr_support' ) ) :
 			'assets/alignments-editor.css'
 		) );
     }
-    add_action( 'after_setup_theme', 'kjellr_support' );
+    add_action( 'after_setup_theme', 'kjell_blocks_support' );
 endif;
 
 /**
  * Enqueue scripts and styles.
  */
-function kjellr_scripts() {
+function kjell_blocks_scripts() {
 
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'kjellr-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'kjell-blocks-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 
 	// Enqueue alignments stylesheet.
-	wp_enqueue_style( 'kjellr-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'kjell-blocks-alignments-style', get_template_directory_uri() . '/assets/alignments-front.css', array(), wp_get_theme()->get( 'Version' ) );
 }
-add_action( 'wp_enqueue_scripts', 'kjellr_scripts' );
+add_action( 'wp_enqueue_scripts', 'kjell_blocks_scripts' );
